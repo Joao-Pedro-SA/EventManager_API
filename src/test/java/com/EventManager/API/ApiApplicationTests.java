7db64523c129995b87bb13e65779f66eea3faafc
João Pedro <joao.pedro.a@live.com>
2026-03-20 00:42:58 -0300

    Feat: first commit

---
+++ src/test/java/com/EventManager/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EventManager.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
