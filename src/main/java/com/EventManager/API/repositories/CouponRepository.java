5ed0205374ec61e7960bb845d2b0143f43174802
João Pedro <joao.pedro.a@live.com>
2026-03-24 08:28:37 -0300

    feat: add custom query methods to event and coupon repositories

--- src/main/java/com/EventManager/API/repositories/CouponRepository.java
+++ src/main/java/com/EventManager/API/repositories/CouponRepository.java
@@ -3,7 +3,10 @@ package com.EventManager.API.repositories;
 import com.EventManager.API.Domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+    List<Coupon> findByEventIdAndValidAfter(UUID eventId, LocalDateTime currentDate);
 }
--- src/main/java/com/EventManager/API/repositories/EventRepository.java
+++ src/main/java/com/EventManager/API/repositories/EventRepository.java
@@ -1,13 +1,31 @@
 package com.EventManager.API.repositories;
 
 import com.EventManager.API.Domain.event.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
 
 
+    @Query("SELECT e FROM Event e LEFT JOIN FETCH e.addresses a WHERE e.date >= :currentDate")
+    public Page<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate, Pageable pageable);
 
+    @Query("SELECT e FROM Event e " +
+            "LEFT JOIN e.addresses a " +
+            "WHERE (:title = ' ' OR e.title LIKE %:title%) AND "+
+            "(:city = ' ' OR e.city LIKE %:city%) AND "+
+            "(:uf = ' ' OR a.uf LIKE %:uf%) AND "+
+            "(e.date >= :startDate AND e.date <= :endDate)")
+    public Page<Event> findFilterEvents(@Param("title") String title,
+                                        @Param("city") String city,
+                                        @Param("uf")String uf,
+                                        @Param("startDate") LocalDateTime startDate,
+                                        @Param("endDate") LocalDateTime endDate,
+                                        Pageable pageable);
 
 }
